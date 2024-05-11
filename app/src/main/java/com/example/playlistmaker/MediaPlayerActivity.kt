4438ac4927f039954b4b73515e67a7da0097f93b
underbreathing <artem-sheverdyaev@mail.ru>
2024-05-12 02:53:20 +0300

    Init commit

--- app/src/main/java/com/example/playlistmaker/MediaPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MediaPlayerActivity.kt
@@ -1,7 +1,12 @@
 package com.example.playlistmaker
 
+import android.media.MediaPlayer
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.Group
@@ -14,11 +19,44 @@ import java.util.Locale
 const val TRACK_INTENT_DATA = "track_intent_data"
 
 class MediaPlayerActivity : AppCompatActivity() {
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private var playerState = State.DEFAULT
+    private lateinit var playButton: ImageButton
+    private lateinit var trackCurrentTime: TextView
+    private lateinit var uiHandler: Handler
+    private lateinit var updatePlayProgressTask: Runnable
+
+    enum class State {
+        DEFAULT,
+        PREPARED,
+        PLAYING,
+        PAUSED
+    }
+
+    private companion object {
+        const val TRACK_CURRENT_TIME_UPDATE_DELAY = 300L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media_player)
+        uiHandler = Handler(Looper.getMainLooper())
 
-        val trackData = intent.getParcelableExtra<Track>(TRACK_INTENT_DATA)
+        val trackData = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(TRACK_INTENT_DATA, Track::class.java) as Track
+        } else {
+            intent.getParcelableExtra<Track>(TRACK_INTENT_DATA) as Track
+        }
+
+        playButton = findViewById(R.id.buttonPlay)
+        playButton.isEnabled = false
+        playButton.setOnClickListener {
+            playButtonControl()
+        }
+        mediaPlayer = MediaPlayer()
+        preparePlayer(trackData.previewUrl)
+
 
         val trackImage = findViewById<ImageView>(R.id.trackArtwork)
         Glide.with(this)
@@ -43,12 +81,68 @@ class MediaPlayerActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.genre).text = trackData?.primaryGenreName
         findViewById<TextView>(R.id.country).text = trackData?.country
-        val releaseDate = trackData?.releaseDate ?: ""
-        if(releaseDate.length > 3){
-            findViewById<TextView>(R.id.year).text = releaseDate.substring(0,4)
+        val releaseDate = trackData.releaseDate ?: ""
+        if (releaseDate.length > 3) {
+            findViewById<TextView>(R.id.year).text = releaseDate.substring(0, 4)
         }
-        findViewById<TextView>(R.id.trackCurrentTime).text = "0.29" // пока прогресса воспроизведения нет здесь заглушка
+        trackCurrentTime = findViewById(R.id.trackCurrentTime)
+        updatePlayProgressTask = object : Runnable {
+            override fun run() {
+                trackCurrentTime.text = SimpleDateFormat(
+                    "mm:ss",
+                    Locale.getDefault()
+                ).format(mediaPlayer.currentPosition)
+                uiHandler.postDelayed(this, TRACK_CURRENT_TIME_UPDATE_DELAY)
+            }
+        }
 
+    }
 
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        uiHandler.removeCallbacks(updatePlayProgressTask)
+        mediaPlayer.release()
+    }
+
+    private fun playButtonControl() {
+        when (playerState) {
+            State.PREPARED, State.PAUSED -> startPlayer()
+            State.PLAYING -> pausePlayer()
+            State.DEFAULT -> {}
+        }
+    }
+
+    private fun preparePlayer(dataSource: String) {
+        mediaPlayer.setDataSource(dataSource)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton.isEnabled = true
+            playerState = State.PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            uiHandler.removeCallbacks(updatePlayProgressTask)
+            trackCurrentTime.text = getString(R.string.mediaplayer_play_progress_start)
+            playerState = State.PREPARED
+            playButton.setImageResource(R.drawable.ic_button_play)
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        uiHandler.post(updatePlayProgressTask)
+        playerState = State.PLAYING
+        playButton.setImageResource(R.drawable.ic_button_pause)
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        uiHandler.removeCallbacks(updatePlayProgressTask)
+        playerState = State.PAUSED
+        playButton.setImageResource(R.drawable.ic_button_play)
     }
 }
